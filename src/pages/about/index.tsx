496a6be3c04767fbb551257224b3c80118625ff1
John Partee <morganpartee@gmail.com>
2021-03-10 09:02:54 -0500

    added copy to index, we'll MD this this weekend

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -24,7 +24,18 @@ const AboutIndex = (props: Props) => {
 
   return (
     <Layout location={props.location} title={siteTitle} navLinks={navLinks} >
-      Some Bullshit here
+      <br />
+      <p># TODO: Make this blog useful {/* This stays lol */}</p>
+      <p>
+        I still can't believe I got this domain name.
+    </p>
+      <p>
+        Tony and I met when we were in the Air Force. I wound up working on satellite dishes for a while, he went off and did network stuff, but we both fell ass backwards into software. We've got weird backgrounds, but they helped make us who we are today.
+    </p>
+      <p>
+        Here we're going to share some of that experience, and try to hand everyone that visits some sensible defaults for stuff like DevSecOps, machine learning, and a ton of resources for falling ass-backwards into this wonderful world like we did.
+    </p>
+      <b>- John</b>
     </Layout>
   )
 }
