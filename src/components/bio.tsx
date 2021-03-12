ed03c200a6a644068a0f315497886e2c5049f1fb
Cheeks <cheeks@butt.io>
2021-03-12 17:55:35 -0500

    Removes dead code

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -1,13 +1,4 @@
-/**
- * Bio component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
-import { graphql, useStaticQuery } from "gatsby"
-import Image from "gatsby-image"
-import React, { ComponentProps, forwardRef, Ref } from "react"
+import React from "react"
 import styled from "styled-components"
 import { useSiteMetadata } from "../hooks/useSiteMetadata"
 import { rhythm } from "../utils/typography"
@@ -16,33 +7,8 @@ const Content = styled.div`
   display: flex;
   margin-bottom: ${rhythm(0.5)};
 `
-
-const GatsbyImage = forwardRef(
-  (props: ComponentProps<typeof Image>, ref: Ref<Image>) => (
-    <Image {...props} ref={ref} />
-  )
-)
-
-const Avatar = styled(GatsbyImage)`
-  border-radius: 100%;
-  margin-bottom: 0;
-  margin-right: ${rhythm(1 / 2)};
-  min-width: 50px;
-`
-
 export const Bio = () => {
   const { social, author } = useSiteMetadata()
-  const data = useStaticQuery(graphql`
-    query BioQuery {
-      avatar: file(absolutePath: { regex: "/sd-profile.png/" }) {
-        childImageSharp {
-          fixed(width: 50, height: 50) {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-    }
-  `)
 
   return (
     <Content>
